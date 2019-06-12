dd705aa5e9ae445f0b56347931b45a9382f61dc6
fed-gren <deveb22@gmail.com>
2019-06-12 19:21:43 +0900

    feat: Header component
    - 모든 페이지에서 공통적으로 사용할 Header 컴포넌트 작성

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/header.scss";
+
+export default ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+};
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "../../styles/layout.scss";
+import Header from "./Header";
 
 export default ({ children }) => {
-  return <div className="layout">{children}</div>;
+  return (
+    <>
+      <div className="layout">
+        <Header title="Portfolio" />
+        {children}
+      </div>
+    </>
+  );
 };
