70b85ae07a69158e14d800faa52bdcc6c011c57c
fed-gren <deveb22@gmail.com>
2019-06-15 23:49:41 +0900

    feat: Gallery component
    - 프로젝트 카드 형태로 모아서 표시할 갤러리 컴포넌트 작성

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,22 +1,56 @@
 import React from "react";
 import "../../styles/gallery.scss";
+import Card from "../components/Card";
 
 export default () => {
+  const projects = [
+    {
+      title: "dummy",
+      thumb: "https://via.placeholder.com/150?text=1"
+    },
+    {
+      title: "dummy",
+      thumb: "https://via.placeholder.com/150?text=2"
+    },
+    {
+      title: "dummy",
+      thumb: "https://via.placeholder.com/150?text=3"
+    },
+    {
+      title: "dummy",
+      thumb: "https://via.placeholder.com/150?text=4"
+    },
+    {
+      title: "dummy",
+      thumb: "https://via.placeholder.com/150?text=5"
+    },
+    {
+      title: "dummy",
+      thumb: "https://via.placeholder.com/150?text=1"
+    },
+    {
+      title: "dummy",
+      thumb: "https://via.placeholder.com/150?text=2"
+    },
+    {
+      title: "dummy",
+      thumb: "https://via.placeholder.com/150?text=3"
+    },
+    {
+      title: "dummy",
+      thumb: "https://via.placeholder.com/150?text=4"
+    },
+    {
+      title: "dummy",
+      thumb: "https://via.placeholder.com/150?text=5"
+    }
+  ];
+
   return (
     <div className="gallery">
-      <div>item</div>
-      <div>item</div>
-      <div>item</div>
-      <div>item</div>
-      <div>item</div>
-      <div>item</div>
-      <div>item</div>
-      <div>item</div>
-      <div>item</div>
-      <div>item</div>
-      <div>item</div>
-      <div>item</div>
-      <div>item</div>
+      {projects.map(project => {
+        return <Card title={project.title} thumb={project.thumb} />;
+      })}
     </div>
   );
 };
