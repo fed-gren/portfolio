f9c2b830d32449fe4cad20e368de887c6e01daac
fed-gren <deveb22@gmail.com>
2019-06-16 23:24:28 +0900

    feat: use api
    - api 서버에서 데이터 가져오도록 수정

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,56 +1,40 @@
 import "../styles/gallery.scss";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
 
 export default () => {
-  const projects = [
-    {
-      title: "dummy",
-      thumb: "https://via.placeholder.com/150?text=1"
-    },
-    {
-      title: "dummy",
-      thumb: "https://via.placeholder.com/150?text=2"
-    },
-    {
-      title: "dummy",
-      thumb: "https://via.placeholder.com/150?text=3"
-    },
-    {
-      title: "dummy",
-      thumb: "https://via.placeholder.com/150?text=4"
-    },
-    {
-      title: "dummy",
-      thumb: "https://via.placeholder.com/150?text=5"
-    },
-    {
-      title: "dummy",
-      thumb: "https://via.placeholder.com/150?text=1"
-    },
-    {
-      title: "dummy",
-      thumb: "https://via.placeholder.com/150?text=2"
-    },
-    {
-      title: "dummy",
-      thumb: "https://via.placeholder.com/150?text=3"
-    },
-    {
-      title: "dummy",
-      thumb: "https://via.placeholder.com/150?text=4"
-    },
-    {
-      title: "dummy",
-      thumb: "https://via.placeholder.com/150?text=5"
-    }
-  ];
+  const [projects, setProjects] = useState([]);
+  const callApi = () => {
+    return fetch("https://gren-apis.herokuapp.com/api/projects")
+      .then(response => response.json())
+      .then(json => json)
+      .catch(err => console.log(err));
+  };
+
+  const getProjects = async () => {
+    const temp = await callApi();
+    //_callApi() 실행완료 전까지 아래 코드는 실행되지 않음 -> ?
+    setProjects(temp);
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  const renderProjects = () => {
+    const temp = projects.map((project, index) => {
+      return <Card
+        title={project.title}
+        thumb={project.thumb}
+        key={index}
+      />
+    })
+    return temp;
+  }
 
   return (
     <div className="gallery">
-      {projects.map((project, index) => {
-        return <Card title={project.title} thumb={project.thumb} key={index} />;
-      })}
+      {projects.length === 0 ? 'Loading' : renderProjects()}
     </div>
   );
 };
