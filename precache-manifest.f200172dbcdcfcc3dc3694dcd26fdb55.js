3cd4a6c91ce5212d890d79e19d7ac01b772234a9
fed-gren <deveb22@gmail.com>
2019-06-19 10:22:04 +0900

    Updates

---
+++ precache-manifest.f200172dbcdcfcc3dc3694dcd26fdb55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbf46937dc810ed6acda49d42a93987",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "58ab88b68500477decb6",
+    "url": "/portfolio/static/css/main.e54603ce.chunk.css"
+  },
+  {
+    "revision": "628d7a5b434def8eb15a",
+    "url": "/portfolio/static/js/2.6421900e.chunk.js"
+  },
+  {
+    "revision": "58ab88b68500477decb6",
+    "url": "/portfolio/static/js/main.11aa7486.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.784d01ab.js"
+  }
+]);
